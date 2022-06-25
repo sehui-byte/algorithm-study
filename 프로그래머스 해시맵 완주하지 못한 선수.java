4683d50b2786772e4fecb52d6f0cc572e11a117f
박병율 <77149239+mikhe123@users.noreply.github.com>
2022-06-26 01:09:24 +0900

    Add files via upload

---
+++ 프로그래머스 해시맵 완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+package com.pantos.vms.biz.test;
+
+import java.util.HashMap;
+
+public class HashMapAthelet {
+
+	public String solution(String[] participant, String[] completion) {
+		String answer = "";
+		HashMap<String, Integer> hm = new HashMap<>();
+		for (String player : participant)
+			hm.put(player, hm.getOrDefault(player, 0) + 1);
+		for (String player : completion)
+			hm.put(player, hm.get(player) - 1);
+		for (String key : hm.keySet()) {
+			if (hm.get(key) != 0) {
+				answer = key;
+				System.out.println(answer);
+				break;
+			}
+		}
+		return answer;
+	}
+}
