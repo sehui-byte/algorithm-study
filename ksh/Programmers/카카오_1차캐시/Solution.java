f41a2f35a3de754a742834c5be456195266d3123
sehui-byte <kimsell1859@gmail.com>
2022-07-04 22:58:01 +0900

    3주차_프로그래머스_카카오1차캐시 linkedHashSet을 이용하여 다시 품

--- ksh/Programmers/카카오_1차캐시/Solution.java
+++ ksh/Programmers/카카오_1차캐시/Solution.java
@@ -1,40 +1,38 @@
 package 카카오_1차캐시;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-// Todo: map으로 풀 필요 X -> Set으로 바꾸기
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
+        final int HIT = 1;
+        final int PASS = 5;
 
-        Map<String, Integer> m = new LinkedHashMap<String, Integer>(cacheSize) {
-            // cacheSize 초과시 오래된 entry 제거 (lease recently used)
-            @Override
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > cacheSize;
-            }
-        };
+        Set<String> hashSet = new LinkedHashSet<String>(cacheSize);
 
         for (String city : cities) {
             // 대소문자 구분 안하므로
             String c = city.toLowerCase();
 
-            if (m.isEmpty()) {
-                m.put(c, m.getOrDefault(c, 0) + 1);
-                answer += 5;
+            if (hashSet.isEmpty()) {
+                hashSet.add(c);
+                answer += PASS;
             } else {
+                // 가장 오래 참조되지 않은 값을 제거
+                if (cacheSize < hashSet.size()) {
+                    hashSet.remove(hashSet.stream().findFirst().get());
+                }
                 //hit (hit하면 맨 뒤로 보내줘서 최신으로 만든다)
-                if (m.containsKey(c)) {
-                    int value = m.get(c);
-                    m.remove(c);
-                    m.put(c, value + 1);
-                    answer += 1;
+                if (hashSet.contains(c)) {
+                    hashSet.remove(c);
+                    hashSet.add(c);
+                    answer += HIT;
                 }
                 //pass
                 else {
-                    m.put(c, m.getOrDefault(c, 0) + 1);
-                    answer += 5;
+                    hashSet.add(c);
+                    answer += PASS;
                 }
             }
         }
